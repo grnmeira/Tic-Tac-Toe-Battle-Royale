67aff70b18635d5721bc79bdc2473f868ea49a4f
Gustavo Meira <grnmeira@gmail.com>
2019-02-28 21:12:38 +0000

    adding board tiles to tic tac toe board model

--- server/ttt-model.hpp
+++ server/ttt-model.hpp
@@ -1,17 +1,96 @@
+#include <vector>
+#include <iostream>
 
+using namespace std;
 
 class TicTacToeBoard
 {
     public:
 
-        enum class Player { X, O };
+        enum class Player: char
+        { 
+            X, 
+            O,
+        };
+
+        class BoardTile
+        {
+            bool _in_use;
+            Player _player;
+
+            public:
+
+                bool operator==(const BoardTile& other)
+                {
+                    return _in_use == other._in_use &&
+                           _player == other._player;
+                }
+
+                bool is_in_use()
+                {
+                    return _in_use;
+                }
+
+                void mark(const Player& player)
+                {
+                    _in_use = true;
+                    _player = player;
+                }
+        };
+
+        enum class PlayResult 
+        {
+            NEXT_TURN,
+            WINNER,
+            DRAW,
+            INVALID_PLAY
+        };
+
+        std::vector<std::vector<BoardTile>> _board;
+
+        TicTacToeBoard(): 
+            _board(3, std::vector<BoardTile>(3, BoardTile())),
+            _player_in_turn(Player::X)
+        {
+        }
 
         Player next_player()
         {
-            return Player::X;
+            return _player_in_turn;
+        }
+
+        PlayResult play(const Player& player, const int x, const int y)
+        {
+            if(_board[x][y].is_in_use())
+                return PlayResult::INVALID_PLAY;
+
+            if(_player_in_turn != player)
+                return PlayResult::INVALID_PLAY;
+
+            _board[x][y].mark(player);
+
+            if(_player_in_turn == Player::X)
+                _player_in_turn = Player::O;
+            else if(_player_in_turn == Player::O)
+                _player_in_turn = Player::X;
+
+            return _check_play_result();
         }
 
     private:
      
         Player _player_in_turn;
+
+        PlayResult _check_play_result()
+        {
+            for(auto col_index = 0; col_index < _board.size(); col_index++)
+            {
+               if(_board[col_index][0].is_in_use() &&
+                  _board[col_index][0] == _board[col_index][1] &&
+                  _board[col_index][1] == _board[col_index][2])
+                   return PlayResult::WINNER;
+            }
+
+            return PlayResult::NEXT_TURN;
+        }
 };
--- server/unit-tests.cpp
+++ server/unit-tests.cpp
@@ -2,11 +2,45 @@
 
 #include "ttt-model.hpp"
 
+typedef TicTacToeBoard TTTBoard;
+
 TEST(TTTBoard, x_is_the_first_player)
 {
-    TicTacToeBoard board;
+    TTTBoard board;
 
-    ASSERT_EQ(board.next_player(), TicTacToeBoard::Player::X);
+    ASSERT_EQ(board.next_player(), TTTBoard::Player::X);
+}
+
+TEST(TTTBoard, o_is_the_next_player)
+{
+    TTTBoard board;
+
+    ASSERT_EQ(board.next_player(), TTTBoard::Player::X);
+
+    auto result = board.play(TTTBoard::Player::X, 1, 1);
+
+    ASSERT_EQ(result, TTTBoard::PlayResult::NEXT_TURN);
+    ASSERT_EQ(board.next_player(), TTTBoard::Player::O);
+}
+
+TEST(TTTBoard, cant_play_same_tile_twice)
+{
+    TTTBoard board;
+
+    ASSERT_EQ(board.next_player(), TTTBoard::Player::X);
+
+    ASSERT_EQ(board.play(TTTBoard::Player::X, 1, 1), TTTBoard::PlayResult::NEXT_TURN);
+    ASSERT_EQ(board.play(TTTBoard::Player::O, 1, 1), TTTBoard::PlayResult::INVALID_PLAY);
+}
+
+TEST(TTTBoard, invalid_player_on_next_turn)
+{
+    TTTBoard board;
+
+    ASSERT_EQ(board.next_player(), TTTBoard::Player::X);
+
+    ASSERT_EQ(board.play(TTTBoard::Player::X, 1, 1), TTTBoard::PlayResult::NEXT_TURN);
+    ASSERT_EQ(board.play(TTTBoard::Player::X, 2, 2), TTTBoard::PlayResult::INVALID_PLAY);
 }
 
 int main(int argc, char **argv)
